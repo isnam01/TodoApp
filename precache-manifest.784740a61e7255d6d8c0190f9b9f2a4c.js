e07854e905bb8bfcba0e7d1f8c20276d86283cb3
mansi <guptamansi199901@gmail.com>
2020-07-05 18:10:45 +0530

    Updates

---
+++ precache-manifest.784740a61e7255d6d8c0190f9b9f2a4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5e54970a0c8e0dd02e7c4728d0cac4",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "dfa564de773b680fcd80",
+    "url": "/TodoApp/static/css/main.af04562d.chunk.css"
+  },
+  {
+    "revision": "13b26ce7d5fc97f09fe9",
+    "url": "/TodoApp/static/js/2.b7912e7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp/static/js/2.b7912e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa564de773b680fcd80",
+    "url": "/TodoApp/static/js/main.2c9edd49.chunk.js"
+  },
+  {
+    "revision": "15df3c4a88ceafa56723",
+    "url": "/TodoApp/static/js/runtime-main.989c0bbe.js"
+  }
+]);
